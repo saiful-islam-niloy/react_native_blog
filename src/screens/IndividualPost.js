a5a806bca760b60e6d19cf5527ca9441a2aa4698
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-10-19 08:06:47 +0600

    individual screen added

---
+++ src/screens/IndividualPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const IndividualPost = () =>{
+    return(
+        <View>
+            <Text>I am individual post screen</Text>
+        </View>
+    );
+}
+
+export default IndividualPost;
