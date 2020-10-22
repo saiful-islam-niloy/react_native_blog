ae11c7559929efa2b0728c47785069d9ba7d4d15
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-10-22 11:37:56 +0600

    blog list can be loaded in home page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react'
+import AsyncStorage from "@react-native-community/async-storage";
+import { View, FlatList } from 'react-native';
 import { Card, Text, Button } from 'react-native-elements';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-const Blog = () => {
-    return (
-        <View>
-            <Card>
-                <Card.Title style={{alignSelf:"flex-start", fontSize:25}}><FontAwesomeIcon icon={faUser} size={20} color={"blue"} /> Author name</Card.Title>
-                <Text>October 29, 2020</Text>
-                <Card.Divider />
-                <Text>This section is for the bolg</Text>
-                <Card.Divider />
-                <Button title={"Like"} type="outline"/>
-                <Button title="Comment" type="outline" />
-            </Card>
-        </View>
-    );
-}
+export default class Blog extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            view: ""
+        }
+    }
 
-export default Blog;
+    componentDidMount() {
+        AsyncStorage.getItem('blogList').then(myData => this.setState({ data: JSON.parse(myData).post }))
+    }
+
+    renderBlog = ({ item }) => (
+        <Card>
+            <Card.Title style={{ alignSelf: "flex-start", fontSize: 25 }}>
+                <FontAwesomeIcon icon={faUser} size={20} color={"blue"} />
+                {item.authorName}
+            </Card.Title>
+            <Text>October 29, 2020</Text>
+            <Card.Divider />
+            <Text>{item.blogData}</Text>
+            <Card.Divider />
+            <Button title={"Like"} type="outline" />
+            <Button title="Comment" type="outline" />
+        </Card>
+    );
+
+    render() {
+        return (
+            <View>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={this.renderBlog}
+                    keyExtractor={item => `${item.id}`}
+                />
+            </View>
+        )
+    }
+}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,34 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Button, Header} from 'react-native-elements';
+import { View } from "react-native";
+import { Header } from 'react-native-elements';
 import { AuthContext } from '../providers/AuthProvider';
 import Blog from '../components/Blog';
 import Post from '../components/Post';
+import { getBlogJson } from '../functions/BlogFunction';
 
 const HomeScreen = (props) => {
+    const blogData = getBlogJson("blogList");
     return (
         <AuthContext.Consumer>
             {(auth) => (<View>
                 <Header
                     leftComponent={{ icon: "menu", color: "#fff", onPress: function () { props.navigation.toggleDrawer() } }}
-                    centerComponent={{text: "My Blog App"}}
+                    centerComponent={{ text: "My Blog App" }}
                     rightComponent={{ icon: "lock-outline", color: "#fff", onPress: function () { auth.setIsLoggedIn(false); auth.setCurrentUser({}); } }}
                 />
 
-                <Post/>
-
+                <Post />
                 <Blog />
-
-
-                {/* <Text>
-                    Welcome {auth.currentUser.name} </Text>
-                <Button
-                    type="outline"
-                    title="Log Out"
-                    onPress={function () {
-                        auth.setIsLoggedIn(false)
-                        auth.setCurrentUser({})
-                    }} /> */}
             </View>)}
         </AuthContext.Consumer>
     );
